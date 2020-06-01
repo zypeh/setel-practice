bf1aace09f1b2c3f8888bcd9232993fa363bdfe9
zypeh <zypeh.geek@gmail.com>
2020-06-01 13:31:13 +0800

    test file added

--- order-app/tests/test.js
+++ order-app/tests/test.js
@@ -1,7 +1,7 @@
 const tap = require('tap')
 const buildFastify = require('../dist').buildFastifyTest
 
-tap.test('GET `/order/9999` route', t => {
+tap.test('Test order not found', t => {
     t.plan(4)
 
     const fastify = buildFastify()
@@ -20,6 +20,56 @@ tap.test('GET `/order/9999` route', t => {
         t.deepEqual(response.json(), {
             error: 'Order does not exist',
             success: false,
+        })
+    })
+})
+
+tap.test('No request on POST endpoint', t => {
+    t.plan(4)
+
+    const fastify = buildFastify()
+
+    // At the end of your tests it is highly recommended to call `.close()`
+    // to ensure that all connections to external services get closed.
+    t.tearDown(() => fastify.close())
+
+    fastify.inject({
+        method: 'POST',
+        url: '/order',
+        payload: undefined,
+    }, (err, response) => {
+        t.error(err)
+        t.strictEqual(response.statusCode, 400)
+        t.strictEqual(response.headers['content-type'], 'application/json; charset=utf-8')
+        t.deepEqual(response.json(), {
+            error: 'Bad Request',
+            message: 'body should be object',
+            statusCode: 400,
+        })
+    })
+})
+
+tap.test('Invalid request for POST endpoint', t => {
+    t.plan(4)
+
+    const fastify = buildFastify()
+
+    // At the end of your tests it is highly recommended to call `.close()`
+    // to ensure that all connections to external services get closed.
+    t.tearDown(() => fastify.close())
+
+    fastify.inject({
+        method: 'POST',
+        url: '/order',
+        payload: {},
+    }, (err, response) => {
+        t.error(err)
+        t.strictEqual(response.statusCode, 400)
+        t.strictEqual(response.headers['content-type'], 'application/json; charset=utf-8')
+        t.deepEqual(response.json(), {
+            error: 'Bad Request',
+            message: 'body should have required property \'id\'',
+            statusCode: 400,
         })
     })
 })
